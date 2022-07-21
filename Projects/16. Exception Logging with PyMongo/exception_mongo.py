1a88fc00ead7dafcc39637194bd34e3c4a2cc91b
Sai Nayunipati <visa423@gmail.com>
2022-07-21 16:44:41 -0400

    Complete task 16

--- Projects/16. Exception Logging with PyMongo/exception_mongo.py
+++ Projects/16. Exception Logging with PyMongo/exception_mongo.py
@@ -1,7 +1,9 @@
-import pymongo
 import logging
+import pymongo
 
-logging.basicConfig(filename="logs.txt", filemode="a")
+logging.basicConfig(filename="logs.txt", filemode="w", level=logging.INFO,
+                    format="%(asctime)s : %(message)s")
+logger = logging.getLogger()
 
 URL = "mongodb://localhost:27017"
 mongoClient = pymongo.MongoClient(URL)
@@ -10,8 +12,21 @@ mycollection = mydb["test_collection"]
 
 print("Hello! This is your simplified MongoDB shell. Enter a command:\n")
 
-command = input("mycollection.")
+method = input("(Omit parentheses and args): mycollection.")
+args = input("\n(Omit the closing parenthesis): mycollection." + method + "(")
 try:
-    eval("mycollection." + command)
-except Exception as e:
-    print("An error has occurred: " + str(e))
+    eval("mycollection." + method + "(" + args + ")")  # pylint: disable=eval-used
+except TypeError as te:
+    logger.info("%s() is not a valid method.", method)
+    logger.exception(te)
+    print("An error occurred. Check the log for more information.")
+except NameError as ne:
+    logger.info("Invalid args: %s(%s)", method, args)
+    logger.exception(ne)
+    print("An error occurred. Check the log for more information.")
+except Exception as e:  # pylint: disable=broad-except
+    logger.info("Some unknown error occurred.")
+    logger.exception(e)
+    print("An error occurred. Check the log for more information.")
+finally:
+    print("\nThe shell has closed.")
